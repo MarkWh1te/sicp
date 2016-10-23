1330176b240d1e098d31809013997a5771baa592
MarkWh1te <iamwh1temark@outlook.com>
2016-10-24 00:58:32 +0800

    fix a version control mistake

--- 2_2_2.rkt
+++ 2_2_2.rkt
@@ -2,10 +2,10 @@
 ;; understand list and cons
 (define a (cons (list 1 2)(list 3 4)))
 (define a_s (cons (list 1 2)(cons 3 (cons 4 '()))))
-;; excerise 2.24
+;; exercise 2.24
 (define w (list 1 (list 2(list 3 4))))
 (display w)
-;; excerise 2.25
+;; exercise 2.25
 (define a1 (list 1 3 (list 5 7) 9))
 (display a1)
 (newline)
@@ -16,7 +16,7 @@
 (newline)
 (define a3 (list 1(list 2 (list 3 (list 4 (list 5 (list 6 7)))))))
 (display (car(cdr(car(cdr(car(cdr(car(cdr(car(cdr(car (cdr a3)))))))))))))
-;; excerise 2.26
+;; exercise 2.26
 (define x (list 1 2 3))
 (define y (list 4 5 6))
 (display (append x y))
@@ -25,5 +25,10 @@
 (newline)
 (display (list x y))
 (newline)
-;; excerise 2.27
+;; exercise 2.18
+(define (revers iterms)
+  (if (null? (cdr(cdr items)))
+             (cons(car(cdr(items)) (cons(car(items))'())))
+             (cons (reverse (cdr items)) (cons(car items)'()))))
+;; exercise 2.27
 
